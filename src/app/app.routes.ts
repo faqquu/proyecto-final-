4e9ab250a9c95d3048313a12b1963bc895af0f4c
unknown <faqquu>
2025-05-07 11:03:05 -0300

    first commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,18 @@
 import { Routes } from '@angular/router';
+import path from 'node:path';
+import { InicioComponent } from './paginas/inicio/inicio.component';
+import { ContactoComponent } from './paginas/contacto/contacto.component';
+import { ProductosComponent } from './paginas/productos/productos.component';
+import { OfertasComponent } from './paginas/ofertas/ofertas.component';
+import { CarritoComponent } from './paginas/carrito/carrito.component';
+import { QuienessomosComponent } from './paginas/quienessomos/quienessomos.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '', redirectTo : '/inicio', pathMatch: 'full'},
+    {path: 'inicio', component:InicioComponent},
+    {path: 'contacto', component:ContactoComponent},
+    {path: 'productos', component:ProductosComponent},
+    {path: 'ofertas', component:OfertasComponent},
+    {path: 'carrito', component:CarritoComponent},
+    {path: 'quienessomos', component:QuienessomosComponent}
+];
---
+++ src/app/paginas/productos/productos.component.ts
@@ -0,0 +1,47 @@
+import { CommonModule } from '@angular/common';
+import { Component, NgModule } from '@angular/core';
+import { CarritoComponent } from '../carrito/carrito.component';
+import { CarritoService } from '../../servicios/carrito.service';
+import { Producto } from '../../modelos/producto.models';
+import { RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'app-productos',
+  imports: [CommonModule, RouterModule],
+  templateUrl: './productos.component.html',
+  styleUrl: './productos.component.css'
+})
+
+export class ProductosComponent {
+  Productos: Producto[] = [
+
+    {
+      id: 1,
+      nombre: 'Hola',
+      descripcion: 'Mundo',
+      precio: 10,
+      imagen: 'https://preview.redd.it/variable-quality-hideo-kojima-thinking-v0-tbztklkehktc1.jpg?width=640&crop=smart&auto=webp&s=61c8fe0e3169b69829af8d919ce32160de9ecfe0',
+      disponibilidad: true
+    },
+
+    {
+      id: 2,
+      nombre: 'radio',
+      descripcion: 'head',
+      precio: 20,
+      imagen: 'https://is1-ssl.mzstatic.com/image/thumb/Music126/v4/dd/50/c7/dd50c790-99ac-d3d0-5ab8-e3891fb8fd52/634904032463.png/1200x630bf-60.jpg',
+      disponibilidad: true
+    }
+  ];
+
+  productos = [];
+
+  constructor(private carritoService: CarritoService) { }
+
+  //metodo para agregar un producto al carrito
+
+  agregar(producto: Producto) {
+    this.carritoService.agregarAlCarrito(producto);
+    alert('Producto agregado al carrito'); //Muestra el mensaje
+  }
+}
